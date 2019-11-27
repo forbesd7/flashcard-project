be8025a5fbb53d59682aab248afccb05e649a7ab
Derek <derek.forbes77@gmail.com>
2019-11-27 18:50:56 +0900

    initial commit

---
+++ client/build/precache-manifest.9fbc63bc939533bc8360c6a302698d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b82c42de98169420b457d1e5bcfdbea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cfe66b2cf01663c4e13",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "19ff66ef8e1ebd2efc2b",
+    "url": "/static/js/2.fc325d15.chunk.js"
+  },
+  {
+    "revision": "4cfe66b2cf01663c4e13",
+    "url": "/static/js/main.08e81509.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/deandemo/react-go-heroku
+
+go 1.13
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20190923054218-35076c1b2bea
+	github.com/gin-gonic/gin v1.5.0
+)
