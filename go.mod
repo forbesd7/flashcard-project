aefea5205f175a8b1b0a8e149add92c3dbfd8206
Derek <derek.forbes77@gmail.com>
2019-11-28 15:02:14 +0900

    database functionality

--- go.mod
+++ go.mod
@@ -5,4 +5,13 @@ go 1.13
 require (
 	github.com/gin-gonic/contrib v0.0.0-20190923054218-35076c1b2bea
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.1.3
+	golang.org/x/crypto v0.0.0-20191122220453-ac88ee75c92c // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/text v0.3.2 // indirect
+	labix.org/v2/mgo v0.0.0-20140701140051-000000000287
 )
--- main.go
+++ main.go
@@ -1,21 +1,100 @@
 package main
 
 import (
-	"github.com/gin-gonic/contrib/static"
-	"github.com/gin-gonic/gin"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
 
-	r := gin.Default()
-	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
-	r.Use(static.Serve("/", static.LocalFile("/build", true)))
-	api := r.Group("/api")
-	api.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://derek:ajninsword@cluster0-swqnm.mongodb.net/test?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	r.Run()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+
+	quickstartDatabase := client.Database("quickstart")
+	podcastsCollection := quickstartDatabase.Collection("podcasts")
+	episodesCollection := quickstartDatabase.Collection("episodes")
+
+	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
+		{"title", "The Polyglot Developer Podcast"},
+		{"author", "Nic Raboy"},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("inserted %v", podcastResult)
+	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "GraphQL for API Development"},
+			{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
+			{"duration", 25},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Progressive Web Application Development"},
+			{"description", "Learn about PWA development with Tara Manicsic."},
+			{"duration", 32},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+
+	cursor, err := episodesCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var episode bson.M
+		if err = cursor.Decode(&episode); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(episode)
+	}
+
+	filterCursor, err := episodesCollection.Find(ctx, bson.M{"duration": 25})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var episodesFiltered []bson.M
+	if err = filterCursor.All(ctx, &episodesFiltered); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(episodesFiltered)
+	fmt.Println("filtered eps")
+
+	fs := http.FileServer(http.Dir("/client/build"))
+	http.Handle("/", fs)
+	log.Println("Listening...")
+	http.ListenAndServe(":8080", nil)
+
 }
