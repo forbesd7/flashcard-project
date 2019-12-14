25bcb63bb9100d4aff9aac845b507968dd94b69d
Derek <derek.forbes77@gmail.com>
2019-12-14 15:11:58 +0900

    changing server file for heroku..?

--- flashcard-project/server/index.js
+++ flashcard-project/server/index.js
@@ -6,9 +6,9 @@ const main = require("../database/database");
 
 app.use(express.static(path.join(__dirname, "../build/")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
