0d68d82c34783c588d4fccce25f59cb09054ab09
Derek <derek.forbes77@gmail.com>
2019-12-14 15:57:01 +0900

    getting mongodb to work with heroku

--- flashcard-project/database/database.js
+++ flashcard-project/database/database.js
@@ -1,23 +1,37 @@
 const mongo = require("mongodb").MongoClient;
 
 const main = async () => {
-  const url = "mongodb://localhost:27017";
+  const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
   const client = new mongo(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
-  await client.connect();
-  await listDatabases(client);
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 };
 
 async function listDatabases(client) {
-  databasesList = await client
+  const databasesList = await client
     .db()
     .admin()
     .listDatabases();
 
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+  const db = await client.db("flashcard");
+
+  const collection = await db.collection("deck1");
+
+  await collection.insertOne({ test2: "FAAACK" });
 }
 
 main();
