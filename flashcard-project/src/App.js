8771cb4f8b6713957c77caab827fe3fbdbbf4cee
Derek <derek.forbes77@gmail.com>
2019-12-17 08:41:14 +0900

    starting to style

--- flashcard-project/src/App.js
+++ flashcard-project/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Decks from "./Components/Decks";
+import HomeScreen from "./Components/HomeScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/homescreen" component={HomeScreen}></Route>
         <Route path="/" component={Decks}></Route>
       </Switch>
     </BrowserRouter>
---
+++ flashcard-project/src/Components/HomeScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Typography, Grid, withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+  container: {
+    width: "100vw",
+    height: "100vw",
+    background: "goldenrod"
+  }
+});
+const HomeScreen = props => {
+  return (
+    <Grid className={props.classes.container} alignItems="center" container>
+      <Grid item>
+        <Typography>Study</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>Make Your Own Deck</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>About</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default withStyles(styles)(HomeScreen);
